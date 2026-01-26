50f2f659be09adbba1b8ca4c2a88665432960cf6
Doujima26 <hdung261204@gmail.com>
2026-01-26 09:30:45 +0700

    them co che kiem tra dk tai khoan

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,3 +9,16 @@ def root():
     with engine.connect() as conn:
         result = conn.execute(text("SELECT 1"))
         return {"db_test": result.scalar()}
+
+if register_role == "admin":
+    if secret_key != ADMIN_REGISTER_KEY:
+        raise HTTPException(403, "Ma xac thuc khong hop le")
+    role = "admin"
+
+elif register_role == "staff":
+    if secret_key != STAFF_REGISTER_KEY:
+        raise HTTPException(403, "Ma xac thuc khong hop le")
+    role = "staff"
+
+else:
+    role = "user"
