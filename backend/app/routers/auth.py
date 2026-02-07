3be124cf1a0868e3f587fdb46cdf8d13f944f57c
Doujima26 <hdung261204@gmail.com>
2026-02-07 13:11:36 +0700

    thêm router login

--- backend/app/routers/auth.py
+++ backend/app/routers/auth.py
@@ -5,13 +5,21 @@ from app.schemas.user import UserCreate, UserResponse
 from app.core.database import get_db
 from app.services.auth_service import register_user
 
+from app.schemas.user import UserLogin
+from app.schemas.token import TokenResponse
+from app.services.auth_service import login_user
 
 router = APIRouter(
     prefix="/auth",
     tags=["Auth"]
 )
 
-
+#router register
 @router.post("/register", response_model=UserResponse)
 def register(user_data: UserCreate, db: Session = Depends(get_db)):
     return register_user(db, user_data)
+
+#router login
+@router.post("/login", response_model=TokenResponse)
+def login(user_data: UserLogin, db: Session = Depends(get_db)):
+    return login_user(db, user_data)
