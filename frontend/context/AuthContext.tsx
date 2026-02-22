eb79de5648aa3fd59371a8e575378b9f5320601d
Doujima26 <hdung261204@gmail.com>
2026-02-23 03:25:09 +0700

    sửa fetch

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -33,7 +33,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     }
 
     try {
-      const res = await fetch("http://127.0.0.1:8000/users/me", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
         headers: {
           Authorization: `Bearer ${token}`,
         },
